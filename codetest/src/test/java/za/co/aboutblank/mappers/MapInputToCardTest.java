f7bb0a809e0ba212a99e8e47123f92b0870cc41d
Anthony Buckland <anthony.buckland@waitrose.co.uk>
2024-04-18 09:53:08 +0200

    Removed weird autoformat in MapInputToCardTest

--- codetest/src/test/java/za/co/aboutblank/mappers/MapInputToCardTest.java
+++ codetest/src/test/java/za/co/aboutblank/mappers/MapInputToCardTest.java
@@ -19,15 +19,39 @@ class MapInputToCardTest {
     void abbreviationToCardTestJokerOnly() throws InvalidCardException {
         var input = List.of("j"); // NOTE: Lower case by intention
         ArrayList<Card> output = sut.abbreviationToCard(input);
-        assertAll("Test if the abbreviated Joker value has been converted to Enum", () -> assertEquals(SuitsEnum.valueOf("JOKER"), output.get(0).getSuit(), "Suit should be JOKER"), () -> assertEquals(ScoresEnum.valueOf("SCORE_JOKER"), output.get(0).getScore(), "Score should be SCORE_JOKER"));
+        assertAll("Test if the abbreviated Joker value has been converted to Enum",
+                () -> assertEquals(SuitsEnum.valueOf("JOKER"), output.get(0).getSuit(),
+                        "Suit should be JOKER"),
+                () -> assertEquals(ScoresEnum.valueOf("SCORE_JOKER"),
+                        output.get(0).getScore(), "Score should be SCORE_JOKER"));
     }
 
     @Test
-    void abbreviationToCardTestSingleValues() throws InvalidCardException {
+    void abbreviationToCardTestDoubleCharValues() throws InvalidCardException {
         var input = List.of("CA", "C2", "S3", "S4", "HK");
         ArrayList<Card> output = sut.abbreviationToCard(input);
 
-        assertAll("Test if all the abbreviated values have been converted to Enums (non ordered)", () -> assertEquals(SuitsEnum.valueOf("CLUBS"), output.get(0).getSuit(), "Suit should be CLUBS"), () -> assertEquals(ScoresEnum.valueOf("SCORE_ACES_LOW"), output.get(0).getScore(), "Score should be SCORE_ACES_LOW"), () -> assertEquals(SuitsEnum.valueOf("CLUBS"), output.get(1).getSuit(), "Suit should be CLUBS"), () -> assertEquals(ScoresEnum.valueOf("SCORE_2"), output.get(1).getScore(), "Score should be SCORE_2"), () -> assertEquals(SuitsEnum.valueOf("SPADES"), output.get(2).getSuit(), "Suit should be SPADES"), () -> assertEquals(ScoresEnum.valueOf("SCORE_3"), output.get(2).getScore(), "Score should be SCORE_3"), () -> assertEquals(SuitsEnum.valueOf("SPADES"), output.get(3).getSuit(), "Suit should be SPADES"), () -> assertEquals(ScoresEnum.valueOf("SCORE_4"), output.get(3).getScore(), "Score should be SCORE_4"), () -> assertEquals(SuitsEnum.valueOf("HEARTS"), output.get(4).getSuit(), "Suit should be HEARTS"), () -> assertEquals(ScoresEnum.valueOf("SCORE_KING"), output.get(4).getScore(), "Score should be SCORE_KING"));
+        assertAll("Test if all the abbreviated values have been converted to Enums (non ordered)",
+                () -> assertEquals(SuitsEnum.valueOf("CLUBS"),
+                        output.get(0).getSuit(), "Suit should be CLUBS"),
+                () -> assertEquals(ScoresEnum.valueOf("SCORE_ACES_LOW"),
+                        output.get(0).getScore(), "Score should be SCORE_ACES_LOW"),
+                () -> assertEquals(SuitsEnum.valueOf("CLUBS"),
+                        output.get(1).getSuit(), "Suit should be CLUBS"),
+                () -> assertEquals(ScoresEnum.valueOf("SCORE_2"),
+                        output.get(1).getScore(), "Score should be SCORE_2"),
+                () -> assertEquals(SuitsEnum.valueOf("SPADES"),
+                        output.get(2).getSuit(), "Suit should be SPADES"),
+                () -> assertEquals(ScoresEnum.valueOf("SCORE_3"),
+                        output.get(2).getScore(), "Score should be SCORE_3"),
+                () -> assertEquals(SuitsEnum.valueOf("SPADES"),
+                        output.get(3).getSuit(), "Suit should be SPADES"),
+                () -> assertEquals(ScoresEnum.valueOf("SCORE_4"),
+                        output.get(3).getScore(), "Score should be SCORE_4"),
+                () -> assertEquals(SuitsEnum.valueOf("HEARTS"),
+                        output.get(4).getSuit(), "Suit should be HEARTS"),
+                () -> assertEquals(ScoresEnum.valueOf("SCORE_KING"),
+                        output.get(4).getScore(), "Score should be SCORE_KING"));
 
     }
 
@@ -36,11 +60,11 @@ class MapInputToCardTest {
         var input = List.of("C10");
         ArrayList<Card> output = sut.abbreviationToCard(input);
 
-        assertAll("Test if all the abbreviated 3 char values have been converted to Enums (non ordered)", () -> assertEquals(SuitsEnum.valueOf("CLUBS"), output.get(0).getSuit(), "Suit should be CLUBS"), () -> assertEquals(ScoresEnum.valueOf("SCORE_10"), output.get(0).getScore(), "Score should be SCORE_10"));
+        assertAll("Test if the abbreviated 3 char values have been converted to Enums", () -> assertEquals(SuitsEnum.valueOf("CLUBS"), output.get(0).getSuit(), "Suit should be CLUBS"), () -> assertEquals(ScoresEnum.valueOf("SCORE_10"), output.get(0).getScore(), "Score should be SCORE_10"));
     }
 
     @Test
-    void abbreviationToCardFourCharTestValues() throws InvalidCardException {
+    void abbreviationToCardFourCharTestValues() {
         var input = List.of("C100");
         assertThrows(InvalidCardException.class, () -> {
             ArrayList<Card> output = sut.abbreviationToCard(input);
